60f7e72ab96bd8167653bcbab7ec4c153e7a55e3
GianfrancoJara <gia.jara@alumnos.duoc.cl>
2023-10-18 16:13:06 -0300

    modified:   client/src/components/Footer.css 	modified:   client/src/components/Footer.jsx 	modified:   client/src/navbar.css

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -40,7 +40,24 @@ const Footer = () => {
   	 			</div>
   	 		</div>
   	 	</div>
+
   	 </div>
+     
+       <hr></hr>
+
+        <div className="sb__footer-below">
+            <div className="sb__footer-copyright">
+                <p>
+                @{new Date().getFullYear()} SrBarber. All right reserved.
+                </p>
+            </div>
+            <div className="sb__footer-below-links">
+                <a href="/Inicio"><div><p>Terminos y condiciones</p></div></a>
+                <a href="/Inicio"><div><p>Privacidad</p></div></a>
+                <a href="/Inicio"><div><p>Seguridad</p></div></a>
+                <a href="/Inicio"><div><p>Cookie Declaration</p></div></a>
+            </div>
+        </div>
   </footer>
     )
 }
