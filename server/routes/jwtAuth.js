b7405f21cb92f0ee09fb7f88d70a84ada3dd375e
VictorGonzalez1 <victo.gonzalezc@duocuc.cl>
2023-11-29 18:37:22 -0300

    Arreglo al token del registro

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -37,7 +37,7 @@ router.post("/register", validInfo, async (req, res) => {
 
         nuevoUsuario.save()
             .then(() => {
-                const token = jwtGenerator(nuevoUsuario.correo);
+                const token = jwtGenerator(nuevoUsuario.correo, nuevoUsuario.nombre, nuevoUsuario.autoridad, nuevoUsuario._id);
                 const response = {
                     newUser: nuevoUsuario,
                     token: token
